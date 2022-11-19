add71eda697f857d0af05ebd981b9d99e1284464
Erdem Meydanli <meydanli@amazon.com>
2022-11-19 00:16:44 +0000

    builder: Reuse binaries
    Reuse existing EIF binaries and exported files if they already
    exist in the container/bin/ directory.
    
    Signed-off-by: Erdem Meydanli <meydanli@amazon.com>

--- scripts/03_build_enclave_apps.sh
+++ scripts/03_build_enclave_apps.sh
@@ -31,16 +31,20 @@ build_enclave_apps() {
 # Exports additional files defined in the enclave manifest file
 # to the artifacts folder.
 export_files() {
-  local enclave_manifest="$BUILDER_CONTAINER_DIR/$project_name/enclave_manifest.json"
+  local enclave_manifest=$1
   # Get the name:tag information of the image.
   local apps_image=$(jq -r '.docker | "\(.image_name):\(.image_tag)"' $enclave_manifest)
   # Get exported files list.
   local exported_files=($(jq -r '[.exports[]]|join(" ")' $enclave_manifest 2> /dev/null))
 
-  for item in ${exported_files[@]}
+  for exp_file in ${exported_files[@]}
   do
-    say "Exporting $item..."
-    copy_from_docker_image $apps_image $item $BUILDER_ARTIFACTS_DIR
+    say "Exporting $exp_file..."
+    [ -f "$BUILDER_ARTIFACTS_DIR/$(basename $exp_file)" ] && {
+      say_warn "$(basename $exp_file) already exists in the bin/ directory."
+      continue;
+    }
+    copy_from_docker_image $apps_image $exp_file $BUILDER_ARTIFACTS_DIR
   done
 }
 
@@ -48,28 +52,39 @@ export_files() {
 # Main
 ####################################################
 main() {
+  local project_name=$1
+
   # Build image builder if it doesn't exist already.
   # This container image has all dependencies installed
   # that we need to build enclave images.
-  docker build -t ${BUILDER_IMAGE} -f ${BUILDER_DOCKERFILE} ${BUILDER_CONTAINER_DIR}
-  mkdir -p ${BUILDER_ARTIFACTS_DIR}
-
-  local project_name=$1
+  docker build -t $BUILDER_IMAGE -f $BUILDER_DOCKERFILE $BUILDER_CONTAINER_DIR
+  mkdir -p $BUILDER_ARTIFACTS_DIR
 
   [[ ! -d $BUILDER_CONTAINER_DIR/$project_name ]] && {
     say_err "Cannot find $project_name under $BUILDER_CONTAINER_DIR directory."
     return $FAILURE
   }
 
+  local enclave_manifest="$BUILDER_CONTAINER_DIR/$project_name/enclave_manifest.json"
+  local eif_file=($(jq -r '.eif_name' $enclave_manifest 2> /dev/null))
+  local skip_eif_build=false
+
+  [ -f "$BUILDER_ARTIFACTS_DIR/$eif_file" ] && {
+    say_warn "$eif_file already exists. Existing EIF file will be reused."
+    skip_eif_build=true;
+  }
+
   # Build enclave apps
-  build_enclave_apps $project_name || {
-    say_err "Enclave apps build failed with error. (Code: $?)"
-    return $FAILURE
+  [[ "$skip_eif_build" != true ]] && {
+    build_enclave_apps $project_name || {
+      say_err "Enclave apps build failed with error. (Code: $?)"
+      return $FAILURE
+    }
   }
 
   # .eif file has already been built and exported to $BUILDER_ARTIFACTS_DIR
   # directory. Let's check if we have any other exports defined in the manifest file.
-  export_files $project_name || {
+  export_files $enclave_manifest || {
     say_err "Enclave apps build failed at export stage with error. (Code: $?)"
     return $FAILURE
   }
